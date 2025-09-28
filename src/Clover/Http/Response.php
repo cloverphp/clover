3fd84401071695f0946a1e3b16c95ac6a7d15b05
Sushil Kumar <sushilkumbhar77@gmail.com>
2025-09-28 12:24:38 +0530

    add Response class

--- src/Clover/Http/Response.php
+++ src/Clover/Http/Response.php
@@ -1,11 +1,28 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Clover\Http;
 
-final class Response {
+use Clover\Interfaces\ResponseInterface;
 
-  public function __construct()
-  {
-      // return $body;
-  }
+final class Response implements ResponseInterface
+{
+    private array $data;
+    private int $status;
+
+    public function send(string $data, int $status = 200): void
+    {
+        http_response_code($status);
+        echo $data;
+        exit;
+    }
+
+    public function json(array $data, int $status = 200): void
+    {
+        http_response_code($status);
+        header("Content-Type: application/json");
+        echo json_encode($data, JSON_PRETTY_PRINT);
+        exit;
+    }
 }
