19a056f7a871b4986f7f2bdea51942f44ad828fd
Sushil Kumar <sushilkumbhar77@gmail.com>
2025-09-28 20:11:19 +0530

    [WIP] Clover PHP

---
+++ public/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Clover PHP</title>
+    <style>
+        h1 {
+            font-size:20px;
+        }
+
+        span {
+            color:green;
+        }
+    </style>
+</head>
+<body>
+
+  <h1>Welcome to <span>Clover PHP!</span></h1>
+
+</body>
+</html>
--- router/routes.php
+++ router/routes.php
@@ -1,2 +1,14 @@
 <?php
 
+use Clover\Clover;
+use Clover\Http\Request;
+use Clover\Http\Response;
+
+$app = new Clover();
+
+$router = $app->router();
+
+$router->get('/public', function (Request $req, Response $res)
+{
+    $res->send('Hello from Clover PHP');
+});
